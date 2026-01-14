bae621ebcf219c882957ce3fd70cea1cec87423c
ivanov <zamt.it@yandex.ru>
2026-01-14 13:24:25 +0300

    Форма Администратора. Готовый дизайн

--- WindowsFormsApp37/Form4.Designer.cs
+++ WindowsFormsApp37/Form4.Designer.cs
@@ -29,7 +29,37 @@ namespace WindowsFormsApp37
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(113, 44);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Добавить";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(131, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(113, 44);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Удалить";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(250, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(170, 44);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "Редактировать";
+            this.button3.UseVisualStyleBackColor = true;
             // 
             // Form4
             // 
@@ -37,6 +67,9 @@ namespace WindowsFormsApp37
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.MediumSpringGreen;
             this.ClientSize = new System.Drawing.Size(584, 561);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.Name = "Form4";
@@ -47,5 +80,9 @@ namespace WindowsFormsApp37
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
